8fce766ef5084137851e925c02c63d4f32191df0
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2016-05-02 13:54:32 -0500

    id numbering matches classic chess board presentation

--- solution.js
+++ solution.js
@@ -5,18 +5,17 @@ var GAME = window.game
 GAME.initialize = function () {
 
   GAME.board = {
-    a1: {}, a2: {}, a3: {}, a4: {}, a5: {}, a6: {}, a7: {},  a8: {},
-    b1: {}, b2: {}, b3: {}, b4: {}, b5: {}, b6: {}, b7: {},  b8: {},
-    c1: {}, c2: {}, c3: {}, c4: {}, c5: {}, c6: {}, c7: {},  c8: {},
-    d1: {}, d2: {}, d3: {}, d4: {}, d5: {}, d6: {}, d7: {},  d8: {},
-    e1: {}, e2: {}, e3: {}, e4: {}, e5: {}, e6: {}, e7: {},  e8: {},
-    f1: {}, f2: {}, f3: {}, f4: {}, f5: {}, f6: {}, f7: {},  f8: {},
+    h1: {}, h2: {}, h3: {}, h4: {}, h5: {}, h6: {}, h7: {},  h8: {},
     g1: {}, g2: {}, g3: {}, g4: {}, g5: {}, g6: {}, g7: {},  g8: {},
-    h1: {}, h2: {}, h3: {}, h4: {}, h5: {}, h6: {}, h7: {},  h8: {}
+    f1: {}, f2: {}, f3: {}, f4: {}, f5: {}, f6: {}, f7: {},  f8: {},
+    e1: {}, e2: {}, e3: {}, e4: {}, e5: {}, e6: {}, e7: {},  e8: {},
+    d1: {}, d2: {}, d3: {}, d4: {}, d5: {}, d6: {}, d7: {},  d8: {},
+    c1: {}, c2: {}, c3: {}, c4: {}, c5: {}, c6: {}, c7: {},  c8: {},
+    b1: {}, b2: {}, b3: {}, b4: {}, b5: {}, b6: {}, b7: {},  b8: {},
+    a1: {}, a2: {}, a3: {}, a4: {}, a5: {}, a6: {}, a7: {},  a8: {}
   } // Is it a little silly to be so explicit? Sure. However, I'm optimizing for low cognitive burden, not less typing.
 
-  // set up the objects
-
+  // set up the board squares
   GAME.board.keys = Object.keys(GAME.board);
   for (var i = 0; i < GAME.board.keys.length; i++) {
     GAME.board[GAME.board.keys[i]].black = false;
@@ -31,7 +30,7 @@ GAME.initialize = function () {
       }
     };
 
-    GAME.board[GAME.board.keys[i]].id = GAME.board.keys[i]; // id replicates key :(
+    GAME.board[GAME.board.keys[i]].id = GAME.board.keys[i];
   }
 
 };
